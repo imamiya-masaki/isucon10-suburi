ecf3c97b8402404f7732ac00492858c74215b41c
imamiya-masaki <imamiyagit@gmail.com>
2021-08-20 08:26:14 +0900

    解説見ながら　＆＆　あと解説で理解しきれてないところがあったので、俺の思考でなんでできないか試す

--- mysql/db/0_Schema.sql
+++ mysql/db/0_Schema.sql
@@ -18,7 +18,8 @@ CREATE TABLE isuumo.estate
     door_width  INTEGER             NOT NULL,
     features    VARCHAR(64)         NOT NULL,
     popularity  INTEGER             NOT NULL,
-    popularity_desc INTEGER AS (-popularity) NOT NULL
+    popularity_desc INTEGER AS (-popularity) NOT NULL,
+    "point" POINT AS (POINT(latitude, longitude)) NOT NULL
 );
 
 CREATE TABLE isuumo.chair
